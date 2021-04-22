db299fc0c537c05a03f5542b145c426d722dd00e
Gabriel Henrique Pereira Soares <ghsoares99795@gmail.com>
2021-04-22 08:57:49 -0300

    .

--- Scripts/Bosses/Cyclops/CyclopsBossHiddenState.cs
+++ Scripts/Bosses/Cyclops/CyclopsBossHiddenState.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using Godot;
 
 public class CyclopsBossHiddenState : State<CyclopsBoss> {
@@ -18,25 +17,4 @@ public class CyclopsBossHiddenState : State<CyclopsBoss> {
             QueryState("Jump");
         }
     }
-=======
-using Godot;
-
-public class CyclopsBossHiddenState : State<CyclopsBoss> {
-    private float currTime {get; set;}
-
-    [Export] public float time = 5f;
-
-    public override void Enter()
-    {
-        currTime = time;
-    }
-
-    public override void PhysicsProcess(float delta)
-    {
-        currTime -= delta;
-        if (currTime <= 0f) {
-            QueryState("Jump");
-        }
-    }
->>>>>>> 169bcf1c52691ab1634d5e844e9575f2036eb400
 }
